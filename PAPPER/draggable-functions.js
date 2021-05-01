bf4604c431ce74381ac957273ea5bb72ec89487e
smanol <smanoliadis@gmail.com>
2021-05-01 12:15:23 +0300

    papper

---
+++ PAPPER/draggable-functions.js
@@ -0,0 +1,63 @@
+var draggables = document.querySelectorAll(".draggable")
+var containers = document.querySelectorAll(".container")
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', (e) => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+
+        if (afterElement == null) {
+            container.appendChild(draggable)
+        }
+
+        else {
+            container.insertBefore(draggable, afterElement)
+        }
+        const saveNotes = (notes) => localStorage.setItem('notes', JSON.stringify(notes))
+
+    })
+
+})
+
+
+const getDragAfterElement = (container, y) => {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2
+
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        }
+        else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+
+document.querySelectorAll("span").forEach(function (node) {
+    node.ondblclick = function () {
+        var val = this.textContent;
+        var input = document.createElement("textarea");
+        input.value = val.trim();
+        input.onblur = function () {
+            var val = this.value.trim();
+            this.parentNode.innerHTML = val;
+        }
+        this.textContent = "";
+        this.appendChild(input);
+        input.focus();
+    }
+});
